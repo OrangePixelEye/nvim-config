f9aaa6417c7f321ccd8ee9a7d1b001526f7eee83
Felipe Batista <felipe.batistaa52@gmail.cpm>
2025-04-03 09:13:24 -0300

    configure shortcuts

--- lua/plugins/tabs.lua
+++ lua/plugins/tabs.lua
@@ -5,5 +5,13 @@ return {
       'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
       'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
     },
+    config = function()
+      require('barbar').setup {
+        -- ... other configurations ...
+      }
+
+      vim.keymap.set('n', '<leader>n', '<Cmd>BufferNext<CR>', { desc = 'Next buffer' })
+      vim.keymap.set('n', '<leader>m', '<Cmd>BufferPrevious<CR>', { desc = 'Previous buffer' })
+    end,
   },
 }
